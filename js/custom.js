81b1b4cec4c0cd962fc99f2d75f52d8a13dc1884
NCTuyen <congtuyenngo51@gmail.com>
2021-10-09 17:02:19 +0700

    update code

--- js/custom.js
+++ js/custom.js
@@ -23,3 +23,22 @@ function openMenu() {
     btn.classList.toggle('open')
 }
 
+function openSignIn() {
+    var signin = document.getElementById('openSignIn')
+    signin.classList.toggle('open-signin')
+    var visible = document.getElementById('Visible')
+    visible.classList.toggle('visible')
+}
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 1992,
+      max: 2011,
+      values: [ 1992, 2011 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "" + ui.values[ 0 ] + " -" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - " + $( "#slider-range" ).slider( "values", 1 ) );
+  } );              //jquery employer
